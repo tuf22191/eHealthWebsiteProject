dd72003d71653b0b534a4f1aa72f9995fe6657a6
marios540 <marios_540@hotmail.com>
2016-03-10 17:00:15 +0000

    an idea of the basic template

--- eHealth/eHealth/settings.py
+++ eHealth/eHealth/settings.py
@@ -89,7 +89,11 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+
+# static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = ( STATIC_PATH,)
